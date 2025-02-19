1b4ca6ec0e62ab4aab7a0be29834894c6b3c6c8a
Lili <lilian.bootcamps@gmail.com>
2025-02-19 11:41:55 -0300

    🎵 Classe responsável pela reprodução de músicas

---
+++ src/main/java/br/com/iphone/application/ReprodutorMusical.java
@@ -0,0 +1,15 @@
+package br.com.iphone.application;
+public class ReprodutorMusical {
+
+    public void tocar() {
+        System.out.println("Tocando música...");
+    }
+
+    public void pausar() {
+        System.out.println("Pausando a música...");
+    }
+
+    public void selecionarMusica(String musica) {
+        System.out.println("Selecionando a música: " + musica);
+    }
+}
