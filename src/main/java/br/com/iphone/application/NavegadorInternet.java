98f4c9f0b3564b39677f15cb7f276a79b71532e8
Lili <lilian.bootcamps@gmail.com>
2025-02-19 11:38:47 -0300

    🌍 Classe responsável por navegação na web

---
+++ src/main/java/br/com/iphone/application/NavegadorInternet.java
@@ -0,0 +1,15 @@
+package br.com.iphone.application;
+public class NavegadorInternet {
+
+    public void exibirPagina(String url) {
+        System.out.println("Exibindo página: " + url);
+    }
+
+    public void adicionarNovaAba() {
+        System.out.println("Adicionando uma nova aba...");
+    }
+
+    public void atualizarPagina() {
+        System.out.println("Atualizando a página...");
+    }
+}
